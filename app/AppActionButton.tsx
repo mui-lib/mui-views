6f9a4529bfe233de923ec6f17ee486d2e7724502
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-01-06 01:56:38 +0800

    [app/AppActionButton] Supported two builtin icons: to add and to edit.

--- app/AppActionButton.tsx
+++ app/AppActionButton.tsx
@@ -4,18 +4,22 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import IconAdd from '@material-ui/icons/Add';
+import IconEdit from '@material-ui/icons/Edit';
 
 export const useStyles = makeStyles({
 	ctnFab: {position: 'absolute', bottom: 50, right: 32},
 });
 
 interface IProps {
+	icon?: 'add' | 'edit';
 	onClick: React.MouseEventHandler;
 }
 
-export const AppActionButton = React.memo(({onClick}: IProps) => {
+export const AppActionButton = React.memo(({icon, onClick}: IProps) => {
 	const cls = useStyles();
 	return (
-		<Fab className={cls.ctnFab} color="primary" onClick={onClick}><IconAdd/></Fab>
+		<Fab className={cls.ctnFab} color="primary" onClick={onClick}>
+			{icon === 'edit' ? <IconEdit/> : <IconAdd/>}
+		</Fab>
 	);
 });
