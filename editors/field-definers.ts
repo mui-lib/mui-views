ae6e548ee7cb67850755aba04a7a8f585b67872e
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-05-18 01:06:38 +0800

    [editors/field-definers] Added support of definer of single selector.

--- editors/field-definers.ts
+++ editors/field-definers.ts
@@ -1,8 +1,12 @@
 //
 
 import {_CommonFields} from 'src/mui-lib/editors/commons';
-import {IInputFieldDefinition, IMultipleSelectorFieldDefinition, ISelectorItem, ISingleSelectorFieldDefinition} from 'src/mui-lib/editors/definitions';
-import {FieldAutoCompleteOff, FieldMarginDense, FieldTypeGroupedCheckboxes, FieldTypeRadio, FieldTypeString, FieldVariantOutlined} from 'src/mui-lib/editors/instances';
+import {IEnvironment, IFieldDefinition, IInputFieldDefinition, IMultipleSelectorFieldDefinition, ISelectorItem, ISingleSelectorFieldDefinition} from 'src/mui-lib/editors/definitions';
+import {
+	FieldAutoCompleteOff, FieldMarginDense,
+	FieldTypeGroupedCheckboxes, FieldTypeRadio, FieldTypeSingleSelector, FieldTypeString,
+	FieldVariantOutlined,
+} from 'src/mui-lib/editors/instances';
 
 // The global options for all applications.
 const mAppFieldDefaultOptions = {
@@ -16,6 +20,7 @@ type funcGetErrorText = (value: string) => string | undefined;
 
 interface IFieldOptions {
 	type?: _CommonFields.IFieldTypeText;
+	getPlaceholder?: (env?: IEnvironment, field?: IFieldDefinition, value?: string) => string | undefined;
 	multiline?: boolean;
 }
 
@@ -23,12 +28,15 @@ interface IFieldDefiners {
 	df: (
 		id: string, label: string, placeholder: string, getErrorText?: funcGetErrorText, extras?: IFieldOptions,
 	) => IInputFieldDefinition;
+	dfSingleSelector: (
+		id: string, label: string, values: ISelectorItem[], getErrorText?: funcGetErrorText, placeholder?: string,
+	) => ISingleSelectorFieldDefinition;
 	dfGroupedCheckBoxes: (
 		id: string, label: string, direction: _CommonFields.IFieldGroupFlexDirection, values: ISelectorItem[], getErrorText?: funcGetErrorText, placeholder?: string,
-	) => IMultipleSelectorFieldDefinition
+	) => IMultipleSelectorFieldDefinition;
 	dfGroupedRadios: (
 		id: string, label: string, direction: _CommonFields.IFieldGroupFlexDirection, values: ISelectorItem[], getErrorText?: funcGetErrorText, placeholder?: string,
-	) => ISingleSelectorFieldDefinition
+	) => ISingleSelectorFieldDefinition;
 }
 
 // A template to build standard field programmatically more fast.
@@ -40,6 +48,14 @@ export const newEditorFieldDefiners = (options?: object): IFieldDefiners => {
 		extras?: { type?: _CommonFields.IFieldTypeText },
 	): IInputFieldDefinition => ({
 		...ops, ...extras, id, label, placeholder,
+		required: Boolean(getErrorText), getErrorText,
+	});
+
+	const dfSingleSelector = (
+		id: string, label: string, values: _CommonFields.ISelectorItem[], getErrorText?: funcGetErrorText, placeholder: string = '',
+	): ISingleSelectorFieldDefinition => ({
+		...ops, type: FieldTypeSingleSelector,
+		id, label, values, placeholder,
 		required: Boolean(getErrorText), getErrorText,
 	});
 
@@ -59,5 +75,5 @@ export const newEditorFieldDefiners = (options?: object): IFieldDefiners => {
 		required: Boolean(getErrorText), getErrorText,
 	});
 
-	return {df, dfGroupedRadios, dfGroupedCheckBoxes};
+	return {df, dfSingleSelector, dfGroupedRadios, dfGroupedCheckBoxes};
 };
